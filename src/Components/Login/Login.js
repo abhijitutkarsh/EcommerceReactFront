18824dc217b0b7d66a4fb5e765f377aa2b3f3eea
Abhijit Utkarsh <abhijitu18@gmail.com>
2022-11-11 04:41:08 +0530

    make it better

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -22,7 +22,7 @@ function Login() {
 	.then((response) => response.json())
 	.then((data) => {
 		console.log(data)
-		// window.location.href = '/'
+		window.location.href = '/'
 	})
       .catch((err) => {
         console.log(err);
